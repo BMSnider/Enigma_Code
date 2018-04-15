0a755bf4ff4c5740aef9a0614a4a5f6693921f37
BMSnider <bertsnider@gmail.com>
2018-04-15 17:04:54 -0500

    Update enigma.py

--- enigma.py
+++ enigma.py
@@ -1,106 +1,83 @@
-def setting(num, list):
-	times = 0
-	while num > times:
-		times += 1
-		q = list.pop(0)
-		list.append(q)
-	return list
-	
-def switch(list):
-	z = raw_input('Do you want to switch to letters? ')
-	while z == 'yes':
-		let1 = raw_input('What\'s the first letter? ')
-		let2 = raw_input('What\'s the second letter? ')
-		x = list.index(let1)
-		y = list.index(let2)
-		list[x], list[y] = let2, let1
-		z = raw_input('Do you want to switch another pair? ')
-	return list
-		
-def enigma():
-	cho = raw_input('Do you want to encrypt or decrypt a message? ')
-	alpha = ['a','b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 
-			'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 
-			'w', 'x', 'y', 'z', ' ', '.']
-	rotor1 = [9, 17, 6, 16, 19, 27, 10, 0, 2, 7, 22, 3, 26, 14, 1, 4, 
-			24, 20, 18, 11, 21, 8, 15, 25, 5, 13, 12, 23]
-	rotor2 = [22, 6, 8, 16, 25, 7, 2, 14, 20, 18, 10, 4, 23, 24, 
-			26, 17, 27, 13, 19, 3, 9, 11, 15, 0, 5, 1, 12, 21]
-	rotor3 = [7, 10, 11, 19, 0, 22, 9, 24, 27, 18, 20, 6, 15, 14, 8, 
-			1, 13, 2, 3, 16, 12, 5, 23, 17, 25, 4, 21, 26]
-	turn = [21, 13, 26, 25, 11, 24, 22, 14, 15, 10, 9, 4, 23, 1, 7, 
-			8, 19, 27, 20, 16, 18, 0, 6, 12, 5, 3, 2, 17]
-	check = []
-	code = ''
-	count1 = 0
-	count2 = 0
-	switch(alpha)
-	setting(int(raw_input('What is the setting for the first rotor? ')), rotor1)
-	setting(int(raw_input('What is the setting for the second rotor? ')), rotor2)
-	setting(int(raw_input('What is the setting for the third rotor? ')), rotor3)
-	#string = raw_input('What is the secret code? ')
-	file = open('c:/users/bert/desktop/input.txt','r+')
-	string = file.read()
-	file.close()
-	if cho == 'encrypt':
-		for x in string:
-			x = str.lower(x)
-			letter = alpha.index(x)
-			change1 = rotor1[letter]
-			change2 = rotor2[change1]
-			change3 = rotor3[change2]
-			change4 = turn[change3]
-			change5 = turn[change4]
-			change6 = rotor3[change5]
-			change7 = rotor2[change6]
-			change8 = rotor1[change7]
-			code = code + alpha[change8]
-			count1 = count1 + 1
-			y = rotor1.pop(0)
-			rotor1.append(y)
-			if count1 == len(alpha) + 1:
-				count2 = count2 + 1
-				z = rotor2.pop(0)
-				rotor2.append(z)
-				count1 = 0
-				if count2 == len(alpha):
-					t = rotor3.pop(0)
-					rotor3.append(t)
-					count2 = 0
-		myfile = open('c:/users/bert/desktop/input.txt','w')
-		myfile.write(str(code))
-		myfile.close()
-		print code
-
-	if cho == 'decrypt':
-		for x in string:
-			letter = alpha.index(x)
-			change1 = rotor1.index(letter)
-			change2 = rotor2.index(change1)		
-			change3 = rotor3.index(change2)
-			change4 = turn.index(change3)
-			change5 = turn.index(change4)
-			change6 = rotor3.index(change5)
-			change7 = rotor2.index(change6)
-			change8 = rotor1.index(change7)
-			code = code + alpha[change8]
-			count1 = count1 + 1
-			y = rotor1.pop(0)
-			rotor1.append(y)
-			if count1 == len(alpha) + 1:
-				count2 = count2 + 1
-				z = rotor2.pop(0)
-				rotor2.append(z)
-				count1 = 0
-				if count2 == len(alpha):
-					t = rotor3.pop(0)
-					rotor3.append(t)
-					count2 = 0
-		myfile = open('c:/users/bert/desktop/input.txt','w')
-		myfile.write(str(code))
-		myfile.close()
-		print code
-
-enigma()d
+import random
+from collections import deque
 
 
+class EnigmaRotor:
+    """Creates rotor for an enigma machine, and manipulates it.
+           -Starting rotor position is used as seed for random number generator .
+           -Rotates rotor.
+           -Output position for next rotor
+           -Output letter for next rotor
+           """
+    
+    def __init__(self, start_pos):
+        '''initializes start position of rotor (0-127)'''
+        random.seed(a = start_pos) #seed based of set rotor position
+        self.rotor = deque([x for x in random.sample(range(128), 128)])
+        self.rotations_count = 0 #starts counter for rotation
+    
+    def rotate_rotor(self):
+        '''rotates rotor and keeps count of how many times it has rotated'''
+        self.rotor.rotate(1)
+        self.rotations_count += 1
+   
+    def next(self, input, is_encrypt = True):
+        '''takes in a input and gives number(position) or letter as output'''
+        if is_encrypt:
+            return self.rotor[input]
+        else:
+            return self.rotor.index(input)
+
+        
+class EnigmaMachine:
+    """Creates Enigma Machine with three rotors, that can decrypt or encrypt.
+    """
+    
+    def __init__(self, start_settings = [0,1,2]):
+        r1, r2, r3 = start_settings
+        self.rotor1 = EnigmaRotor(r1)
+        self.rotor2 = EnigmaRotor(r2)
+        self.rotor3 = EnigmaRotor(r3)
+        self.mirror = EnigmaRotor(r1+r2+r3)
+    
+    def crypt(self, message, is_encrypt = True):
+        """ outputs encrypted message or decrypted, based of boolean"""
+        encrypted_message = ''
+        for letter in message:
+            encrypted_message += self.next_letter(letter, is_encrypt)
+        return encrypted_message
+        
+    def next_letter(self, letter, is_encrypt = True):
+        '''gives output of letter from letter input'''
+        pos = ord(letter)
+        #first pass
+
+        pos = self.rotor1.next(pos, is_encrypt)
+        pos = self.rotor2.next(pos, is_encrypt)
+        pos = self.rotor3.next(pos, is_encrypt)
+        #mirrir
+        pos = self.mirror.next(pos, is_encrypt)
+        #second pass
+        pos = self.rotor3.next(pos, is_encrypt)
+        pos = self.rotor2.next(pos, is_encrypt)
+        pos = self.rotor1.next(pos, is_encrypt)
+        
+        #Handles rotation of rotors
+        rotor1_count = self.rotor1.rotate_rotor()
+        if rotor1_count == 128:
+            rotor2_count = self.rotor2.rotate_rotor()
+            rotor1.rotations_count = 0
+            if rotor2_count == 128:
+                rotor3_count = self.rotor3.rotate_rotor()
+                rotor2.rotations_count = 0
+                
+        return chr(pos) # letter output
+
+    
+    
+if __name__ == "__main__":
+    enc = EnigmaMachine([2,3,4]).crypt('ffffff', True)
+    print(enc)
+    de = EnigmaMachine([2,3,4]).crypt(enc, False)
+    print(de)
+        
